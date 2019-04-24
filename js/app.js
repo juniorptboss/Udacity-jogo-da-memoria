bf2491908e120d7437b4e07aedacde64723607a7
Sergio <junior.ptboss@gmail.com>
2019-04-24 20:11:02 +0100

    atualização index.html app.css app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,222 @@
-/*
- * Create a list that holds all of your cards
- */
+// declarando variaveis
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+let openedCards = [];
+let matchedCard = document.getElementsByClassName("match");
+
+//variavel de movimentos do jogo
+let moves = 0;
+
+let refreshHTML = function(target, value) {
+	return target.innerHTML = value;
+};
+
+// variavel de parabens
+const popup = document.getElementById("parabens");
+
+// numero de estrelas
+let numEstrela = function() {
+	this.stars = document.querySelectorAll(".fa-star");
+};
+
+numEstrela.prototype.rate = function() {
+	if(moves > 12 && moves < 18) {
+		this.stars[2].classList.remove("shine");
+	} else if(moves > 18) {
+		this.stars[1].classList.remove("shine");
+	}
+};
+
+numEstrela.prototype.restart = function() {
+	for(var i=0; i<this.stars.length; i++) {
+		this.stars[i].classList.add("shine");
+	}
+};
+
+let stars = new numEstrela();
+
+// declarando tempo
+const timer = document.querySelector(".timer");
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// contador
+let CounterSet = function(moves) {
+	this.target = document.querySelector(".counter");
+	refreshHTML(this.target, moves);
+};
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+CounterSet.prototype.add = function() {
+	moves++;
+	refreshHTML(this.target, moves);
+};
+
+CounterSet.prototype.restart = function() {
+	moves = 0;
+	refreshHTML(this.target, moves);
+};
+
+let counter = new CounterSet(moves);
+
+
+// declarando o tempo inicial, recebendo o valor 0
+let second = {
+	value: 0,
+	label: " segs"
+};
+
+let minute = {
+	value: 0,
+	label: " mins "
+};
+
+let interval;
+
+window.onload = startGame();
+
+// loop para adicionar cada cartão
+for(var i = 0; i < cards.length; i++) {
+	cards[i].addEventListener("click", displayCard);
+	cards[i].addEventListener("click", cardOpen);
+	cards[i].addEventListener("click", congratulations);
+}
+
+// butão de reiniciar
+document.querySelector(".restart").addEventListener("click", startGame);
+
+// função shuffle com base em https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	return array;
+}
 
-    return array;
+// Iniciando Jogo
+function startGame() {
+	cards = shuffle(cards);
+	for(var i=0; i<cards.length; i++) {
+		document.querySelector(".deck").innerHTML = "";
+		[].forEach.call(cards, function(item) {
+			document.querySelector(".deck").appendChild(item);
+		});
+		cards[i].classList.remove("show", "open", "match", "disabled");
+	}
+	counter.restart();
+	stars.restart();
+	resetTimer();
+}
+
+// function para mostrar, desbilitar e abrir.
+function displayCard() {
+	this.classList.toggle("open");
+	this.classList.toggle("show");
+	this.classList.toggle("disabled");
+}
+
+// verificação de cards.
+function cardOpen() {
+	openedCards.push(this);
+	if(openedCards.length === 2) {
+		counter.add();
+		stars.rate();
+		startTimer();
+		if(openedCards[0].type === openedCards[1].type) {
+			matched();
+		} else {
+			unmatched();
+		}
+	}
+}
+
+// Quando unir os cards certos
+function matched() {
+	for(var i=0; i<openedCards.length; i++) {
+		openedCards[i].classList.add("match", "disabled");
+		openedCards[i].classList.remove("show", "open", "no-event");
+	}
+	openedCards = [];
+}
+
+// Quando os cards unidos forem errados
+function unmatched() {
+	for(var i=0; i<openedCards.length; i++) {
+		openedCards[i].classList.add("unmatched");
+	}
+	disable();
+	setTimeout(function() {
+		for(var i=0; i<openedCards.length; i++) {
+			openedCards[i].classList.remove("show", "open", "no-event", "unmatched");
+		}
+		enable();
+		openedCards = [];
+	}, 1100);
+}
+
+// desabilitando cards
+function disable() {
+	for(var i = 0; i < cards.length; i++) {
+		cards[i].classList.add("disabled");
+	}
+}
+
+// ativando os cartões, menos os combinados.
+function enable() {
+	for(var i = 0; i < cards.length; i++) {
+		if(!cards[i].classList.contains("match")) {
+			cards[i].classList.remove("disabled");
+		};
+	}
+}
+
+// iniciando o tempo
+function startTimer() {
+	if(moves == 1) {
+		interval = setInterval(function() {
+			second.value++;
+			if(second.value == 60) {
+				minute.value++;
+				second.value = 0;
+			}
+			refreshTimer();
+		}, 1000);
+	}
+}
+
+// atualizar o tempo no HTML
+function refreshTimer() {
+	timer.innerHTML = minute.value + minute.label + second.value + second.label;
+}
+
+// resetar o tempo
+function resetTimer() {
+	second.value = 0;
+	minute.value = 0;
+	refreshTimer();
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// congratulations popup when all cards match
+function congratulations() {
+	if(matchedCard.length == 16) {
+		clearInterval(interval);
+		popup.classList.add("show");
+		document.getElementById("total-moves").innerHTML = moves;
+		document.getElementById("total-time").innerHTML = timer.innerHTML;
+		document.getElementById("numEstela").innerHTML = document.querySelector(".stars").innerHTML;
+		closePopup();
+	};
+}
+
+// close popup function on play again button
+function closePopup() {
+	document.getElementById("jogar-novamente").addEventListener("click", function() {
+		popup.classList.remove("show");
+		startGame();
+	});
+}
